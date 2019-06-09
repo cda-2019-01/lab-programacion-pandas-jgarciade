dbba8194671a8e4f96b9e8912e631104d3c79d14
jose garcia <jgarciade@unal.edu.co>
2019-06-09 18:00:25 +0000

    adding solution to q02.py

--- q02.py
+++ q02.py
@@ -1,5 +1,15 @@
 ##
-## Imprima el promedio de la _c2 por cada letra de la _c1 
+## Imprima el promedio de la _c2 por cada letra de la _c1
 ## de la tabla tbl0
-## 
+##
 
+import pandas as pd
+
+def main(file_path, sep='\t'):
+    file_to_process = pd.read_csv(file_path, sep=sep)
+    grouped = file_to_process.groupby('_c1')
+    return grouped.mean()['_c2']
+
+if __name__ == "__main__":
+    result = main('tbl0.tsv')
+    print(result)
