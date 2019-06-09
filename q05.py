a37758c147caed6a9bc570b0d4caaa332ba0fa32
jose garcia <jgarciade@unal.edu.co>
2019-06-09 18:19:38 +0000

    adding solution to q05.py

--- q05.py
+++ q05.py
@@ -1,5 +1,14 @@
 ##
-## Imprima la suma de la _c2 por cada letra de la _c1 
+## Imprima la suma de la _c2 por cada letra de la _c1
 ## de la tabla tbl0
-## 
+##
 
+import pandas as pd
+
+def main(file_path, sep='\t'):
+    file_to_process = pd.read_csv(file_path, sep=sep)
+    return file_to_process.groupby('_c1').sum()['_c2']
+
+if __name__ == "__main__":
+    result = main('tbl0.tsv')
+    print(result)
