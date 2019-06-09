e681ab9019cfe1ae5f432d0b1f8757ded98e5f08
jose garcia <jgarciade@unal.edu.co>
2019-06-09 17:56:48 +0000

    adding solution to q01.py

--- q01.py
+++ q01.py
@@ -1,4 +1,15 @@
 ##
-## Imprima la cantidad de registros por cada letra 
+## Imprima la cantidad de registros por cada letra
 ## de la columna _c1 de la tabla tbl0
-## 
+##
+
+import pandas as pd
+
+def main(file_path, sep='\t'):
+    file_to_process = pd.read_csv(file_path, sep=sep)
+    grouped = file_to_process.groupby('_c1')
+    return grouped.size()
+
+if __name__ == "__main__":
+    result = main('tbl0.tsv')
+    print(result)
