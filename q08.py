7429ba2a1991dfef7597cfbb6bd5659cfd159796
jose garcia <jgarciade@unal.edu.co>
2019-06-09 18:38:04 +0000

    adding solution to q08.py

--- q08.py
+++ q08.py
@@ -1,4 +1,14 @@
 ##
-## Agregue el año como una columna a la tabla tbl0.tsv 
-## 
+## Agregue el año como una columna a la tabla tbl0.tsv
+##
 
+import pandas as pd
+
+def main(file_path, sep='\t'):
+    file_to_process = pd.read_csv(file_path, sep=sep)
+    file_to_process['ano'] = file_to_process['_c3'].map(lambda x:x.split('-')[0])
+    return file_to_process
+
+if __name__ == "__main__":
+    result = main('tbl0.tsv')
+    print(result)
